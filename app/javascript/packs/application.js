41c20ee32535695b683571112d32655bb4291534
Dixon <dixon@zionstar.in>
2024-05-04 17:48:17 +0530

    Implement basic login

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "bootstrap"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+require("jquery")
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,25 +4,24 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: { name: "",
+      post users_path,params:{ user: { name: "",
                                email: "user@example.com",
                                password:            "foo",
-                               password_confirmation: "bar"}
+                               password_confirmation: "bar"}}
     end
     assert_template 'users/new'
-    #assert_select 'div#error_explanation'
-    #assert_select 'div.field_with_errors'
   end
   test "valid signup information" do
     get signup_path
-    assert_no_difference 'User.count',1 do
-      post users_path, user: { name: "",
-                               email: "user@example.com",
-                               password:            "password",
-                               password_confirmation: "password"}
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
     #assert_select 'div#error_explanation'
     #assert_select 'div.field_with_errors'
   end
